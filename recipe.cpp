9b2b38f52d2f77e64c488a65dc89fe94f9bca3d2
denisesarona <135094209+denisesarona@users.noreply.github.com>
2024-05-28 14:57:18 +0800

    Created Menu for Recipe Pal

---
+++ recipe.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int opt;
+    
+    cout << "+---------------------------------------------------------+" << endl;
+    cout << "|               Welcome to RecipePal!                     |" << endl;
+    cout << "|        Organize your Recipes in an Instant              |" << endl;
+    cout << "+---------------------------------------------------------+" << endl;
+    cout << "| [1] Add a Recipe                                        |" << endl;
+    cout << "| [2] Update a Recipe                                     |" << endl;
+    cout << "| [3] Search for a Recipe                                 |" << endl;
+    cout << "| [4] Delete a Recipe                                     |" << endl;
+    cout << "| [5] Check the Existing Recipes                          |" << endl;
+    cout << "| [6] Exit                                                |" << endl;
+    cout << "+---------------------------------------------------------+" << endl;
+    cout << endl;
+    cout << "  Enter Option: ";
+    cin >> opt;
+}
