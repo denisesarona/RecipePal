a8869bcb4ed32ead03666b872313f30b511513f1
denisesarona <135094209+denisesarona@users.noreply.github.com>
2024-05-28 15:01:49 +0800

    Added filestream and created switch case for menu options

--- recipe.cpp
+++ recipe.cpp
@@ -1,29 +1,59 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 using namespace std;
 
-int main(){
+struct Recipe
+{
+    string name;
+    vector<string> ingredients;
+    vector<string> procedure;
+    string cooking_time;
+    string difficulty_level;
+    string category;
+};
 
+Recipe recipes[100]; // Array of Recipe struct to store multiple recipes
+int opt = 0, count = 0;
+
+int main()
+{
     cout << ",------.              ,--.              ,------.         ,--. \n"
-        "|  .--. ' ,---.  ,---.`--' ,---.  ,---. |  .--. ' ,--,--.|  | \n"
-        "|  '--'.'| .-. :| .--',--.| .-. || .-. :|  '--' |' ,-.  ||  | \n"
-        "|  |\\  \\ \\   --.\\ `--.|  || '-' '\\   --.|  | --' \\ '-'  ||  | \n"
-        "`--' '--' `----' `---'`--'|  |-'  `----'`--'      `--`--'`--' \n"
-        "                          `--'                                 \n";
+            "|  .--. ' ,---.  ,---.`--' ,---.  ,---. |  .--. ' ,--,--.|  | \n"
+            "|  '--'.'| .-. :| .--',--.| .-. || .-. :|  '--' |' ,-.  ||  | \n"
+            "|  |\\  \\ \\   --.\\ `--.|  || '-' '\\   --.|  | --' \\ '-'  ||  | \n"
+            "`--' '--' `----' `---'`--'|  |-'  `----'`--'      `--`--'`--' \n"
+            "                          `--'                                 \n";
 
-    int opt;
+    loadRecipes();
+    while (true)
+    {
+        clearScreen();
+        cout << "+---------------------------------------------------------+" << endl;
+        cout << "|               Welcome to RecipePal!                     |" << endl;
+        cout << "|        Organize your Recipes in an Instant              |" << endl;
+        cout << "+---------------------------------------------------------+" << endl;
+        cout << "| [1] Add a Recipe                                        |" << endl;
+        cout << "| [2] Update a Recipe                                     |" << endl;
+        cout << "| [3] Search for a Recipe                                 |" << endl;
+        cout << "| [4] Delete a Recipe                                     |" << endl;
+        cout << "| [5] Check the Existing Recipes                          |" << endl;
+        cout << "| [6] Exit                                                |" << endl;
+        cout << "+---------------------------------------------------------+" << endl;
+        cout << endl;
+        cout << "  Enter Option: ";
+        cin >> opt;
 
-    cout << "+---------------------------------------------------------+" << endl;
-    cout << "|               Welcome to RecipePal!                     |" << endl;
-    cout << "|        Organize your Recipes in an Instant              |" << endl;
-    cout << "+---------------------------------------------------------+" << endl;
-    cout << "| [1] Add a Recipe                                        |" << endl;
-    cout << "| [2] Update a Recipe                                     |" << endl;
-    cout << "| [3] Search for a Recipe                                 |" << endl;
-    cout << "| [4] Delete a Recipe                                     |" << endl;
-    cout << "| [5] Check the Existing Recipes                          |" << endl;
-    cout << "| [6] Exit                                                |" << endl;
-    cout << "+---------------------------------------------------------+" << endl;
-    cout << endl;
-    cout << "  Enter Option: ";
-    cin >> opt;
+        switch (opt)
+        {
+            case 1: addRecipe(); break;
+            case 2: updateRecipe(); break;
+            case 3: searchRecipe(); break;
+            case 4: deleteRecipe(); break;
+            case 5: viewRecipes(); break;
+            case 6: exit(0); break;
+            default: cout << "Invalid Input" << endl; break;
+        }
+    }
 }
